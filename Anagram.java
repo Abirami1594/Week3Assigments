4cdb6b102ead88062392a9826789e0f9bd84b426
Abirami1594 <109016312+Abirami1594@users.noreply.github.com>
2022-07-29 13:30:43 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,26 @@
+package week3.day1;
+import java.util.Arrays; 
+
+public class Anagram {
+	public static void main(String[] args) {
+		String text1="stops";
+		String text2="potss";
+		int len1= text1.length();
+		int len2 = text2.length();
+		if (len1 == len2) {
+			System.out.println("The Length of the given strings are same");
+		}
+		char[] a = text1.toCharArray();
+		char[] b= text2.toCharArray();
+		Arrays.sort(a);
+		System.out.println(a);
+		Arrays.sort(b);
+		System.out.println(b);
+		if (Arrays.equals(a, b)) {
+		System.out.println("The given strings are same");
+		
+	}else {
+		System.out.println("The given strings are not same");
+	}
+	}
+}
---
+++ ChangeOddIndexToUpperCase.java
@@ -0,0 +1,21 @@
+package week3.day1;
+
+public class ChangeOddIndexToUpperCase {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		String test ="changeme";
+		char[] charArray = test.toCharArray();
+		for (int i=0; i< charArray.length; i++) {
+			if(i%2 ==0) {
+				 charArray[i] = Character.toUpperCase(charArray[i]);
+				 System.out.println(charArray[i]);
+			}else {
+				System.out.println(charArray[i]);
+			}
+		}
+
+	}
+
+}
