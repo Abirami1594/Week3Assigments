5bfb3ab38e07ed9f9cd76755331b2a77496e3ae2
Abirami1594 <109016312+Abirami1594@users.noreply.github.com>
2022-08-02 13:26:11 +0530

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,17 @@
+package week3.day2;
+
+public class AxisBank extends BankInfo {
+	public void deposit() {
+		System.out.println("Deposit with interest 7.5%");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AxisBank obj= new AxisBank();
+		obj.deposit();
+		obj.fixed();
+		obj.saving();
+
+	}
+
+}
---
+++ BankInfo.java
@@ -0,0 +1,17 @@
+package week3.day2;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("My savings");
+	}
+	public void fixed() {
+		System.out.println("FD with interst 7.5%");
+		
+	}
+	public void deposit() {
+		System.out.println("Deposit with interst 7.5%");
+	}
+	public static void main(String[] args) {
+}
+
+}
